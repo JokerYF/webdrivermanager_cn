8b3917aaadf412016d0706d68a8e6e0ff752d1a4
贾泓 <jiahong@lenovocloud.com>
2023-09-21 14:29:51 +0800

    fix: 修改chrome老版本时没有win64的chromedriver的问题

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='webdrivermanager_cn',
     python_requires='>=3.7',
-    version='0.1.1',
+    version='0.1.2',
     install_requires=[
         'requests',
         'packaging'
