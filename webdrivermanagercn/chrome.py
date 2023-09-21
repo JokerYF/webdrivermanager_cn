ace2ca109ea473eb74a23841ff32ab6e8b2408d9
贾泓 <jiahong@lenovocloud.com>
2023-09-21 14:06:20 +0800

    fix: 修改参数名

--- webdrivermanagercn/chrome.py
+++ webdrivermanagercn/chrome.py
@@ -2,8 +2,8 @@ from webdrivermanagercn.drivers.chrome import ChromeDriver
 
 
 class ChromeDriverManager(ChromeDriver):
-    def __init__(self, version=None, root_dir=None):
-        super().__init__(version=version, root_dir=root_dir)
+    def __init__(self, version=None, path=None):
+        super().__init__(version=version, path=path)
 
     def install(self):
         return super().install()
