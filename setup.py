be38c703024f2a8a9ed8a0ed480bfe186f7ab12d
御风 <eternallyyf@163.com>
2023-09-25 14:48:53 +0800

    fix: 解决wheel包中没有项目的问题

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version='0.2.2',
     author_email='eternallyyf@163.com',
     url="https://gitee.com/Joker_JH/webdrivermanagercn",
-    packages=setuptools.find_packages(include=['webdrivermanager_cn']),
+    packages=setuptools.find_packages(include=['webdrivermanager_cn*']),
     install_requires=[
         'requests',
         'packaging'
