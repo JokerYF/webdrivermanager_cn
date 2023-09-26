674d8eefe55ce7fc07bb4f3cc493ff6a7e3d935f
御风 <eternallyyf@163.com>
2023-09-26 14:42:06 +0800

    feat: 补充readme文档，setup.py参数

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 import setuptools
 
+with open("README.md", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
 setuptools.setup(
-    name='webdrivermanager_cn',
-    python_requires='>=3.7',
-    version='0.3.0',
-    author_email='eternallyyf@163.com',
+    name="webdrivermanager_cn",
+    python_requires=">=3.7",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    version="0.3.1",
+    author="御风",
+    author_email="eternallyyf@163.com",
     url="https://gitee.com/Joker_JH/webdrivermanagercn",
-    packages=setuptools.find_packages(include=['webdrivermanager_cn*']),
-    install_requires=[
-        'requests',
-        'packaging'
-    ],
+    packages=setuptools.find_packages(include=["webdrivermanager_cn*"]),
+    install_requires=["requests", "packaging"],
 )
