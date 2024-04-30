69ec0b03e8c825e60f9540f177bf1a65171f7872
御风 <eternallyyf@163.com>
2024-04-30 15:56:26 +0800

    docs: 修改注释

--- webdrivermanager_cn/chrome.py
+++ webdrivermanager_cn/chrome.py
@@ -13,6 +13,9 @@ class ChromeDriverManager:
         """
         ChromeDriver管理器
         :param version:
+            latest: 最新的ChromeDriver版本号
+            current: 根据当前Chrome版本号，自动获取对应的ChromeDriver版本号
+            xxx.xxx.xxx.xxx: 指定的具体ChromeDriver版本号
         :param path:
         """
         self.chromedriver = ChromeDriver(version=version, path=path)
