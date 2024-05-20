b1cc374e84213bdfc97eb59abc9259f14a632e74
御风 <eternallyyf@163.com>
2024-05-20 16:11:33 +0800

    feat: 新download下载方式

--- webdrivermanager_cn/core/download_manager.py
+++ webdrivermanager_cn/core/download_manager.py
@@ -4,6 +4,7 @@
 import os
 
 import requests as requests
+import urllib3
 
 from webdrivermanager_cn.core.log_manager import wdm_logger
 
@@ -20,15 +21,10 @@ class DownloadManager:
         :param down_path:
         :return:
         """
-        wdm_logger().debug(f'开始执行下载: {url}')
-        response = requests.get(url, timeout=15)
-        wdm_logger().debug(f'url: {url} - {response.status_code}')
-        response.raise_for_status()
-        wdm_logger().debug(f'本地下载路径: {down_path}')
+        wdm_logger().debug(f'开始执行下载: {url} - 本地下载路径: {down_path}')
         os.makedirs(down_path, exist_ok=True)
         file_path = os.path.join(down_path, self.get_filename_by_url(url))
-        with open(file_path, "wb") as f:
-            f.write(response.content)
+        self.__load_file(url, file_path)
         return file_path
 
     @staticmethod
@@ -40,3 +36,23 @@ class DownloadManager:
         """
         url_parser = url.split("/")
         return url_parser[-1]
+
+    @staticmethod
+    def __load_file(url, down_path):
+        """
+        从指定的url中下载文件到指定目录中
+        :param url:
+        :param down_path:
+        :return:
+        """
+        http = urllib3.PoolManager()
+        response = http.request('GET', url, preload_content=False)
+
+        with open(down_path, 'wb') as out_file:
+            while True:
+                data = response.read(4096)  # 每次读取的块大小，可根据需要调整
+                if not data:
+                    break
+                out_file.write(data)
+
+        response.release_conn()
