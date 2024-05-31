f282da7fdb45602e212a2bb98367a02eb1925e27
御风 <eternallyyf@163.com>
2024-05-31 10:07:49 +0800

    commit

---
+++ webdrivermanager_cn/core/mirror_url.py
@@ -0,0 +1,13 @@
+# ChromeDriver 源地址
+ChromeDriverUrl = 'https://registry.npmmirror.com/-/binary/chromedriver'
+ChromeDriverUrlNew = 'https://registry.npmmirror.com/-/binary/chrome-for-testing'
+ChromeDriver = 'https://googlechromelabs.github.io/chrome-for-testing'
+ChromeDriverApiNew = 'https://gitee.com/Joker_JH/DriverRelease/raw/master/ChromeDriverLastVersion.json'
+
+# Firefox 驱动源地址
+GeckodriverUrl = 'https://registry.npmmirror.com/-/binary/geckodriver'
+GeckodriverApi = 'https://api.github.com/repos/mozilla/geckodriver/releases'
+GeckodriverApiNew = 'https://gitee.com/Joker_JH/DriverRelease/raw/master/GeckodriverLastVersion.json'
+
+# Edge 驱动源地址
+EdgeDriverUrl = 'https://msedgedriver.azureedge.net'
