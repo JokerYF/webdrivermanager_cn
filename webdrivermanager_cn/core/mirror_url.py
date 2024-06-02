3d7249f0759d7d6657c86c0beba0da04c1921d67
御风 <eternallyyf@163.com>
2024-06-02 14:39:05 +0800

    feat: 增加华为镜像url

--- webdrivermanager_cn/core/mirror_url.py
+++ webdrivermanager_cn/core/mirror_url.py
@@ -1,13 +1,22 @@
-# ChromeDriver 源地址
-ChromeDriverUrl = 'https://registry.npmmirror.com/-/binary/chromedriver'
-ChromeDriverUrlNew = 'https://registry.npmmirror.com/-/binary/chrome-for-testing'
-ChromeDriver = 'https://googlechromelabs.github.io/chrome-for-testing'
-ChromeDriverApiNew = 'https://gitee.com/Joker_JH/DriverRelease/raw/master/ChromeDriverLastVersion.json'
+class PublicMirror:
+    ChromeDriverApiNew = 'https://gitee.com/Joker_JH/DriverRelease/raw/master/ChromeDriverLastVersion.json'
+    GeckodriverApiNew = 'https://gitee.com/Joker_JH/DriverRelease/raw/master/GeckodriverLastVersion.json'
 
-# Firefox 驱动源地址
-GeckodriverUrl = 'https://registry.npmmirror.com/-/binary/geckodriver'
-GeckodriverApi = 'https://api.github.com/repos/mozilla/geckodriver/releases'
-GeckodriverApiNew = 'https://gitee.com/Joker_JH/DriverRelease/raw/master/GeckodriverLastVersion.json'
 
-# Edge 驱动源地址
-EdgeDriverUrl = 'https://msedgedriver.azureedge.net'
+class AliMirror:
+    # ChromeDriver 源地址
+    ChromeDriverUrl = 'https://registry.npmmirror.com/-/binary/chromedriver'
+    ChromeDriverUrlNew = 'https://registry.npmmirror.com/-/binary/chrome-for-testing'
+    ChromeDriver = 'https://googlechromelabs.github.io/chrome-for-testing'
+
+    # Firefox 驱动源地址
+    GeckodriverUrl = 'https://registry.npmmirror.com/-/binary/geckodriver'
+    GeckodriverApi = 'https://api.github.com/repos/mozilla/geckodriver/releases'
+
+    # Edge 驱动源地址
+    EdgeDriverUrl = 'https://msedgedriver.azureedge.net'
+
+
+class HuaweiMirror:
+    ChromeDriverUrl = 'https://repo.huaweicloud.com/chromedriver/'
+    GeckodriverUrl = 'https://repo.huaweicloud.com/geckodriver/'
