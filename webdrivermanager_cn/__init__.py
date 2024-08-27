ecc23fd214caa818b062eba818014dc3cf6e2046
御风 <eternallyyf@163.com>
2024-08-27 18:00:08 +0800

    feat: 增加Geckodriver下载

--- webdrivermanager_cn/__init__.py
+++ webdrivermanager_cn/__init__.py
@@ -1,9 +1,15 @@
 from webdrivermanager_cn.chrome import ChromeDriverManager
 from webdrivermanager_cn.chrome import ChromeDriverManagerAliMirror
 from webdrivermanager_cn.chrome import ChromeDriverManagerHuaweiMirror
+from webdrivermanager_cn.geckodriver import GeckodriverManager
+from webdrivermanager_cn.geckodriver import GeckodriverManagerAliMirror
+from webdrivermanager_cn.geckodriver import GeckodriverManagerHuaweiMirror
 
 __all__ = [
     'ChromeDriverManager',
     'ChromeDriverManagerAliMirror',
-    'ChromeDriverManagerHuaweiMirror'
+    'ChromeDriverManagerHuaweiMirror',
+    'GeckodriverManager',
+    'GeckodriverManagerAliMirror',
+    'GeckodriverManagerHuaweiMirror',
 ]
