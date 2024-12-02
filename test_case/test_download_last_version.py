135e28e527cde1fca81fdf4e0828fe6e10f8d537
御风 <eternallyyf@163.com>
2024-12-02 15:13:03 +0800

    test: 优化test case

--- test_case/test_download_last_version.py
+++ test_case/test_download_last_version.py
@@ -7,30 +7,33 @@ base_path = os.getcwd()
 
 
 class TestDownloadLastVersion:
-    def test_download_chromedriver(self):
+    cm = ChromeDriverManager(path=base_path)
+    gm = GeckodriverManager(path=base_path)
+
+    @staticmethod
+    def teardown_method():
+        shutil.rmtree(os.path.join(base_path, '.webdriver'))
+
+    def test_download_chromedriver_by_ali(self):
         # ali
-        dm = ChromeDriverManager(path=base_path)
-        dm.set_ali_mirror()
-        path = dm.install()
+        self.cm.set_ali_mirror()
+        path = self.cm.install()
         assert os.path.exists(path), 'ChromeDriver下载失败'
-        shutil.rmtree(os.path.join(base_path, '.webdriver'))
 
+    def test_download_chromedriver_by_huawei(self):
         # huawei
-        dm.set_huawei_mirror()
-        path = dm.install()
+        self.cm.set_huawei_mirror()
+        path = self.cm.install()
         assert os.path.exists(path), 'ChromeDriver下载失败'
-        shutil.rmtree(os.path.join(base_path, '.webdriver'))
 
-    def test_download_geckodriver(self):
+    def test_download_geckodriver_by_ali(self):
         # ali
-        dm = GeckodriverManager(path=base_path)
-        dm.set_ali_mirror()
-        path = dm.install()
+        self.gm.set_ali_mirror()
+        path = self.gm.install()
         assert os.path.exists(path), 'Geckodriver下载失败'
-        shutil.rmtree(os.path.join(base_path, '.webdriver'))
 
+    def test_download_geckodriver_by_huawei(self):
         # huawei
-        dm.set_huawei_mirror()
-        path = dm.install()
+        self.gm.set_huawei_mirror()
+        path = self.gm.install()
         assert os.path.exists(path), 'Geckodriver下载失败'
-        shutil.rmtree(os.path.join(base_path, '.webdriver'))
